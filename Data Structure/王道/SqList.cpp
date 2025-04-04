6c397a1592b675842b5658ff870593462cbdf058
Faith889 <149290007+Faith889@users.noreply.github.com>
2025-04-04 21:39:26 +0800

    Update SqList.cpp

--- Data Structure/王道/SqList.cpp
+++ Data Structure/王道/SqList.cpp
@@ -333,6 +333,36 @@ int MinPosInt(int A[],int n){
 	return i+1;
 }
 
+//14
+//#define INT_MAX 0x7fffffff
+int abs_(int a){
+	if(a<0){
+		return -a;
+	}else{
+		return a;
+	}
+}
+bool xls_min(int a,int b,int c){
+	if(a<=b&&a<=c){
+		return true;
+	}else{
+		return false;
+	}
+}
+int findMinofTrip(int A[],int n,int B[],int m,int C[],int p){
+	int i=0,j=0,k=0,D,D_min=INT_MAX;
+	while(i<n&&j<m&&k<p&&D_min>0){
+		D=abs_(A[i]-B[j])+abs_(B[j]-C[k])+abs_(C[k]-A[i]);
+		if(D<D_min){
+			D_min=D;
+		}
+		if(xls_min(A[i],B[j],C[k])) i++;
+		else if(xls_min(B[j],C[k],A[i])) j++;
+		else k++;
+	}
+	return D_min;
+}
+
 int main(){
 	/*05
 	SqList L;
@@ -362,6 +392,12 @@ int main(){
 	int A1[]={0,5,5,3,5,7,5,5},A2[]={0,5,5,3,5,1,5,7};
 	cout<<Majority(A1,8)<<endl<<Majority(A2,8);
 	*/
+	/*13
 	int A1[]={-5,3,2,3},A2[]={1,2,3};
 	cout<<MinPosInt(A1,4)<<endl<<MinPosInt(A2,3);
+	*/
+	/*14
+	int S1[]={-1,0,9},S2[]={-25,-10,10,11},S3[]={2,9,17,30,41};
+	cout<<findMinofTrip(S1,3,S2,4,S3,5);
+	*/
 }
